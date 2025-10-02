9f720ecbdac838693f0db1b02ee1a796aa7b5b7f
mrcyclo <vipboysanhdieu@gmail.com>
2025-10-02 23:45:13 +0700

    Update cache headers for tile image to fix painted pixel not showing after submit

--- src/routes/pixel.ts
+++ src/routes/pixel.ts
@@ -69,7 +69,9 @@ export default function (app: App) {
 			const imageBuffer = await pixelService.getTileImage(tileX, tileY);
 
 			res.setHeader("Content-Type", "image/png");
-			res.setHeader("Cache-Control", "public, max-age=300");
+			res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate")
+            res.setHeader("Pragma", "no-cache")
+            res.setHeader("Expires", "0")
 			return res.send(imageBuffer);
 		} catch (error) {
 			console.error("Error generating tile image:", error);
