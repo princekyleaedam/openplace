ad6cdabf8e95e07ecde9ad989d467fd11997dcfd
lulz <39673284+Udyz@users.noreply.github.com>
2025-10-10 19:00:09 +0700

    Add files via upload

---
+++ prisma/migrations/20251010114618_add_region/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - Added the required column `lat` to the `Region` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lon` to the `Region` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `pixel` ADD COLUMN `regionCityId` INTEGER NULL,
+    ADD COLUMN `regionCountryId` INTEGER NULL;
+
+-- AlterTable
+ALTER TABLE `region` ADD COLUMN `lat` DOUBLE NOT NULL,
+    ADD COLUMN `lon` DOUBLE NOT NULL;
+
+-- CreateIndex
+CREATE INDEX `Pixel_regionCityId_idx` ON `Pixel`(`regionCityId`);
+
+-- CreateIndex
+CREATE INDEX `Pixel_regionCountryId_idx` ON `Pixel`(`regionCountryId`);
