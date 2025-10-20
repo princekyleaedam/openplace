7872433991ecad286ed92fc5fca6970f5b480f4c
lulz <39673284+Udyz@users.noreply.github.com>
2025-10-21 03:03:15 +0700

    stupid move

--- prisma/migrations/20251020132538_init/migration.sql
+++ prisma/migrations/20251020132538_init/migration.sql
@@ -1,18 +1,3 @@
--- DropIndex
-DROP INDEX IF EXISTS `Pixel_paintedAt_idx` ON `pixel`;
-
--- DropIndex
-DROP INDEX IF EXISTS `Pixel_paintedAt_regionCityId_idx` ON `pixel`;
-
--- DropIndex
-DROP INDEX IF EXISTS `Pixel_paintedAt_regionCountryId_idx` ON `pixel`;
-
--- DropIndex
-DROP INDEX IF EXISTS `Pixel_regionCityId_idx` ON `pixel`;
-
--- DropIndex
-DROP INDEX IF EXISTS `Pixel_regionCountryId_idx` ON `pixel`;
-
 -- AlterTable
 ALTER TABLE `profilepicture` MODIFY `url` TEXT NOT NULL;
 
