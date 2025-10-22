8eff2c511120d6915060fcda6ab1d36ba7fe7941
Adam Demasi <adam@hbang.com.au>
2025-10-23 01:37:26 +1100

    Head stuff

--- frontend2/nuxt.config.ts
+++ frontend2/nuxt.config.ts
@@ -9,7 +9,76 @@ export default defineNuxtConfig({
 	},
 
 	app: {
-		baseURL: "/"
+		baseURL: "/",
+
+		head: {
+			title: "openplace",
+
+			htmlAttrs: {
+				lang: "en"
+			},
+
+			meta: [
+				{
+					name: "viewport",
+					content: "width=device-width, initial-scale=1"
+				},
+				{
+					name: "color-scheme",
+					content: "light dark"
+				},
+				{
+					name: "twitter:card",
+					content: "summary_large_image"
+				},
+				{
+					property: "og:type",
+					content: "website"
+				},
+				{
+					property: "og:site_name",
+					content: "openplace"
+				},
+				{
+					property: "og:title",
+					content: "openplace"
+				},
+				{
+					property: "og:image",
+					content: "/img/og-image.png"
+				},
+				{
+					name: "description",
+					content: "openplace is a free unofficial open source backend for wplace."
+				},
+				{
+					name: "og:description",
+					property: "og:description",
+					content: "openplace is a free unofficial open source backend for wplace."
+				}
+			],
+
+			link: [
+				{
+					rel: "preconnect",
+					href: "https://tiles.openfreemap.org"
+				},
+				{
+					rel: "icon",
+					sizes: "96x96",
+					href: "/img/favicon-96x96.png"
+				},
+				{
+					rel: "apple-touch-icon",
+					sizes: "180x180",
+					href: "/img/apple-touch-icon.png"
+				},
+				{
+					rel: "manifest",
+					href: "/site.webmanifest"
+				}
+			]
+		}
 	},
 
 	devServer: {
