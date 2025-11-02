45b3f30166926075f73eb723bb97e02642d7b7aa
lulz <39673284+Udyz@users.noreply.github.com>
2025-11-02 11:19:05 +0700

    drop indexes pixel

---
+++ prisma/migrations/20251102041731_drop_indexes_pixel/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX IF EXISTS `Pixel_paintedAt_idx` ON `pixel`;
+
+-- DropIndex
+DROP INDEX IF EXISTS `Pixel_paintedAt_regionCityId_idx` ON `pixel`;
+
+-- DropIndex
+DROP INDEX IF EXISTS `Pixel_paintedAt_regionCountryId_idx` ON `pixel`;
+
+-- DropIndex
+DROP INDEX IF EXISTS `Pixel_regionCityId_idx` ON `pixel`;
+
+-- DropIndex
+DROP INDEX IF EXISTS `Pixel_regionCountryId_idx` ON `pixel`;
